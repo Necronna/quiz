d827eb9ea830e5a096736913426275836d0e868c
Alina Antonova <87584246+Necronna@users.noreply.github.com>
2023-10-13 01:32:18 +0600

    First comm

---
+++ main.js
@@ -0,0 +1,149 @@
+const questions = [
+	{
+		question: "Какой язык работает в браузере?",
+		answers: ["Java", "C", "Python", "JavaScript"],
+		correct: 4,
+	},
+	{
+		question: "Что означает CSS?",
+		answers: [
+			"Central Style Sheets",
+			"Cascading Style Sheets",
+			"Cascading Simple Sheets",
+			"Cars SUVs Sailboats",
+		],
+		correct: 2,
+	},
+	{
+		question: "Что означает HTML?",
+		answers: [
+			"Hypertext Markup Language",
+			"Hypertext Markdown Language",
+			"Hyperloop Machine Language",
+			"Helicopters Terminals Motorboats Lamborginis",
+		],
+		correct: 1,
+	},
+	{
+		question: "В каком году был создан JavaScript?",
+		answers: ["1996", "1995", "1994", "все ответы неверные"],
+		correct: 2,
+	},
+];
+
+// Находим элементы
+const headerContainer = document.querySelector("#header");
+const listContainer = document.querySelector("#list");
+const submitBtn = document.querySelector("#submit");
+
+// Переменные игры
+let score = 0; // кол-во правильных ответов
+let questionIndex = 0; //текущий вопрос
+
+clearPage()
+showQuestion()
+submitBtn.onclick = checkAnswer;
+
+// Очистим html разметку
+function clearPage() {
+    headerContainer.innerHTML = '';
+    listContainer.innerHTML = '';
+}
+
+// Показать вопрос
+function showQuestion() {
+    //Вопрос
+    const headerTemplate = `<h2 class="title">%title%</h2>`
+    const title = headerTemplate.replace('%title%', questions[questionIndex]['question']);
+    headerContainer.innerHTML = title;
+
+    //Варианты ответов
+    let answerNumber = 1;
+    for (answerText of questions[questionIndex]['answers']) {
+        const questionTemplate = 
+            `<li>
+                <label>
+                    <input value ="%number%" type="radio" class="answer" name="answer" />
+                    <span>%answer%</span>
+                </label>
+            </li>`
+
+        const answerHTML = questionTemplate
+                            .replace('%answer%', answerText)
+                            .replace('%number%', answerNumber);
+
+        listContainer.innerHTML += answerHTML;
+        answerNumber++;
+    }
+}
+
+// Проверить ответ
+function checkAnswer() {
+    // Найти выбранную радио-кнопку
+    const checkedRadio = listContainer.querySelector('input[type="radio"]:checked');
+
+    // Если не выбран ответ - функцию необходимо прервать и убрать фокус с кнопки
+    if(!checkedRadio){
+        submitBtn.blur();
+        return
+    }
+
+    // Узнаем номер ответа пользователя
+    const userAnswer = parseInt(checkedRadio.value);
+
+    // Если ответил верно - увеличиваем счет
+    const correctAnswer = questions[questionIndex]['correct'];
+
+    if (userAnswer === questions[questionIndex]['correct']) {
+        score++;
+    }
+
+    if(questionIndex !== questions.length -1){
+        questionIndex++;
+        clearPage();
+        showQuestion();
+        return
+    } else {
+        clearPage();
+        showResults();
+
+    }
+}
+
+function showResults() {
+    const resultsTemplate =     `
+    <h2 class="title">%title%</h2>
+    <h3 class="summary">%message%</h3>
+    <p class="result">%result%</p>
+    `;
+
+    let title, message;
+
+    // Варианты заголовков и текста
+    if (score === questions.length) {
+        title = "Поздравляем! 👨‍💻 ";
+        message = "Вы ответили верно на все вопросы! 💯🔥";
+    } else if ((score * 100) / questions.length >= 50) {
+        title = "Неплохой результат! 😎 ";
+        message = "Вы дали более половины правильных ответов 👌";
+    } else {
+        title = "Стоит постараться 😉";
+        message = "Пока у вас меньше половины правильных ответов 📚";
+    }
+
+    // Результат
+    let result = `${score} из ${questions.length}`; 
+
+    // Финальное сообщение
+    const finalMessage = resultsTemplate
+                            .replace("%title%", title)
+                            .replace("%message%", message)
+                            .replace("%result%", result);
+
+    headerContainer.innerHTML = finalMessage;
+    
+    // Меняем кнопку на "Играть снова"
+    submitBtn.blur();
+    submitBtn.innerText = 'Начать заново';
+    submitBtn.onclick = ()=>{history.go()};
+}
